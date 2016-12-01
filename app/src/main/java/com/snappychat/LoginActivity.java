874e6889725011627edf432766b8f129e71eab97
Revanth Penugonda <revanthpenugonda@gmail.com>
2016-12-01 01:12:05 -0800

    Need to remove these changes

--- app/src/main/java/com/snappychat/LoginActivity.java
+++ app/src/main/java/com/snappychat/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -18,6 +19,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -97,6 +104,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             public void onSuccess(LoginResult loginResult) {
                 Log.d("Facebook Login", "facebook:onSuccess:" + loginResult);
                 Log.d("Facebook Login", loginResult.getAccessToken().getToken().toString());
+
             }
 
             @Override
@@ -142,7 +150,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         //Call Back Facebook SDK
         callbackManager.onActivityResult(requestcode,resultcode,data);
     }
-
 
 
     @Override
