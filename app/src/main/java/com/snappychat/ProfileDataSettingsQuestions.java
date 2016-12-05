acdef859374c02c83905575b2e6105c7649bc56e
Revanth Penugonda <revanthpenugonda@gmail.com>
2016-12-05 12:09:44 -0800

    Adding ProfileView Fragments

--- app/src/main/java/com/snappychat/ProfileDataSettingsQuestions.java
+++ app/src/main/java/com/snappychat/ProfileDataSettingsQuestions.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ProileDataUserQuestions.OnFragmentInteractionListener} interface
+ * {@link ProfileDataSettingsQuestions.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link ProileDataUserQuestions#newInstance} factory method to
+ * Use the {@link ProfileDataSettingsQuestions#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProileDataUserQuestions extends Fragment {
+public class ProfileDataSettingsQuestions extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class ProileDataUserQuestions extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public ProileDataUserQuestions() {
+    public ProfileDataSettingsQuestions() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class ProileDataUserQuestions extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ProileDataUserQuestions.
+     * @return A new instance of fragment ProfileDataSettingsQuestions.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProileDataUserQuestions newInstance(String param1, String param2) {
-        ProileDataUserQuestions fragment = new ProileDataUserQuestions();
+    public static ProfileDataSettingsQuestions newInstance(String param1, String param2) {
+        ProfileDataSettingsQuestions fragment = new ProfileDataSettingsQuestions();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class ProileDataUserQuestions extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_proile_data_prof_loc, container, false);
+        return inflater.inflate(R.layout.fragment_profile_data_settings_questions, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
