85d0b19f2840ca2e835cf4f4f00e70ff760e5464
Jelson Santos <jesantos0527@gmail.com>
2016-12-10 00:01:28 -0800

    modifying chat conversation activity

--- app/src/main/java/com/snappychat/friends/ChatConverstationsActivity.java
+++ app/src/main/java/com/snappychat/friends/ChatConverstationsActivity.java
@@ -6,20 +6,23 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.snappychat.R;
+import com.snappychat.model.User;
+
+import static com.snappychat.MainActivity.USER_LOGGED_IN;
 
 /**
  * Created by Jelson on 12/8/16.
  */
 
-public class ChatConverstationsActivity extends AppCompatActivity {
-
+public class ChatConverstationsActivity extends AppCompatActivity implements ChatConverstationsFragment.OnListFragmentInteractionListener{
+    private Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_conversations);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_chat_conversations_container);
+        fragment = fm.findFragmentById(R.id.fragment_chat_conversations_container);
 
         if (fragment == null) {
             fragment = new ChatConverstationsFragment();
@@ -27,6 +30,13 @@ public class ChatConverstationsActivity extends AppCompatActivity {
                     .add(R.id.fragment_chat_conversations_container, fragment)
                     .commit();
         }
+
+    }
+
+    @Override
+    public void onDeleteChatConversation(String creator, String receiver) {
+        ChatConverstationsFragment f = (ChatConverstationsFragment) fragment;
+        f.deleteChatConversation(creator, receiver);
 
     }
 }
